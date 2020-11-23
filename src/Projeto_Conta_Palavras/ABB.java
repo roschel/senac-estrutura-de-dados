635a955f6514027abf6fab80d5e3648d8c4d059f
phncosta <phncosta@outlook.com>
2020-11-23 00:28:50 -0300

    feat: Collator Implementation/Regex Special Chars - ED

--- src/Projeto_Conta_Palavras/ABB.java
+++ src/Projeto_Conta_Palavras/ABB.java
@@ -5,32 +5,42 @@
  */
 package Projeto_Conta_Palavras;
 
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 
 public class ABB {
 
     private No raiz;
 
-    //Cria uma árvore vazia
+    List<String> listaFrequenciaPalavras;
+
+    // Cria uma árvore vazia
     public ABB() {
         this.raiz = null;
+
+        if (listaFrequenciaPalavras == null) {
+            listaFrequenciaPalavras = new ArrayList<>();
+        }
     }
 
-    //Verifica se a árvore está vazia
+    // Verifica se a árvore está vazia
     public boolean vazia() {
         return this.raiz == null;
     }
 
     public int insere(String elemento) {
         elemento = verificaChar(elemento);
-        
+
         No novo = new No(elemento, null, null, 1);
         No pai = null;
         No p = raiz;
-        
-        //Buscar onde inserir o novo nó
+
+        // Buscar onde inserir o novo nó
         while (p != null) {
             pai = p;
             if (elemento.compareTo(p.getElemento()) == 0) {
@@ -43,7 +53,7 @@ public class ABB {
                 p = p.getDir();
             }
         }
-        //Verifica se a árvore está vazia
+        // Verifica se a árvore está vazia
         if (vazia()) {
             raiz = novo;
             return raiz.getContador();
@@ -75,24 +85,24 @@ public class ABB {
                 return p.setContador();
             }
         }
-        return 0; //não achou
+        return 0; // não achou
     }
-    
+
     public String verificaChar(String palavra) {
         Pattern pattern = Pattern.compile("[^a-zA-Z0-9áàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ]");
         int inicio = 0, fim;
         Matcher buscador = pattern.matcher(palavra);
-        
-        while(buscador.find()) {
+
+        while (buscador.find()) {
             fim = buscador.start();
-            
-            if(inicio != fim) {
+
+            if (inicio != fim) {
                 palavra = palavra.substring(inicio, fim);
             } else {
                 inicio = buscador.end();
             }
         }
-        if(inicio != palavra.length()) {
+        if (inicio != palavra.length()) {
             fim = palavra.length();
             palavra = palavra.substring(inicio, fim);
         }
@@ -106,14 +116,28 @@ public class ABB {
     public void inOrdem(No p) {
         if (p != null) {
             inOrdem(p.getEsq());
-            System.out.println(p.getElemento() + " - " + p.getContador());
+
+            listaFrequenciaPalavras.add(p.getElemento() + " - " + Integer.toString(p.getContador()));
+
             inOrdem(p.getDir());
         }
     }
 
     @Override
     public String toString() {
-        return "ABB{" + "raiz=" + raiz + '}';
+        return " ABB { " + " raiz = " + raiz + '}';
     }
 
+    public void mostrarFrequenciaPalavras() {
+        System.out.println("Frequência de Palavras: \n");
+
+        // Classe comparadora de String (Local Sensitive)
+        Collator comparador = Collator.getInstance(new Locale("pt"));
+        comparador.setStrength(Collator.SECONDARY);
+        Collections.sort(listaFrequenciaPalavras, comparador);
+
+        for (String item : listaFrequenciaPalavras) {
+            System.out.println(item);
+        }
+    }
 }
